1bbeb85ea6ebed6c9b7293fcee770d2a43a9214f
moran <11608162+moran-xiao@user.noreply.gitee.com>
2024-07-14 18:38:04 +0800

    练习

---
+++ test7.14/test.cc
@@ -0,0 +1,40 @@
+//AcWing 1012. 友好城市
+
+#include <iostream>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+const int N = 5e3+10;
+
+using PII = pair<int,int>;
+int dp[N];
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<PII> v;
+    for(int i = 1; i <= n; i++)
+    {
+        int a,b;
+        cin >> a >> b;
+        v.push_back({a,b});
+    }
+    sort(v.begin(),v.end());
+    for(int i = 0; i < n; i++)
+    {
+        dp[i] = 1;
+        for(int j = 0; j < i; j++)
+        {
+            if(v[i].second > v[j].second) dp[i] = max(dp[i],dp[j]+1);
+        }
+    }
+    int res = 0;
+    for(int i = 0; i < n; i++)
+    {
+        res = max(res,dp[i]);
+    }
+    cout << res << endl;
+    return 0;
+}

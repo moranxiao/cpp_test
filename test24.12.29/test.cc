df4f30cbe20cdb108cf516c9a7a06c63ba2d28d7
moranxiao <yyyjka@foxmail.com>
2024-12-29 09:59:53 +0800

    练习

---
+++ test24.12.29/test.cc
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+        for(auto e : nums)
+        {
+            res ^= e;
+        }
+        return res;
+    }
+};

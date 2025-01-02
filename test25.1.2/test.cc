52bbea8d0f720acc7fc64b8e4630f43ed0e1bf8f
moranxiao <yyyjka@foxmail.com>
2025-01-02 19:57:47 +0800

    练习

--- test25.1.2/test.cc
+++ test25.1.2/test.cc
@@ -4,27 +4,28 @@
 
 using namespace std;
 
-vector<int> nums;
-int maxCount = 1;
+vector<int> nums; //存众数有哪些
+int maxCount = 1; //存最大的重数
 
 void SearchCountMax(int* arr,int l,int r)
 {
-    if(r - l + 1 < maxCount) return;
+    if(r - l + 1 < maxCount) return; //如果区间个数小于最大重数吗，没有必要处理了
     int mid = (l + r) >> 1;
-    int i = l;
-    while(arr[i] != arr[mid]) i++;
+    int i = l; //初始化i
+    while(arr[i] != arr[mid]) i++; //找到第一个等于arr[mid]的元素
     int j = i;
-    while(j <= r && arr[j] == arr[mid]) j++;
-    if(j-i == maxCount) nums.push_back(arr[mid]);
-    else if(j-i > maxCount) 
+    while(j <= r && arr[j] == arr[mid]) j++; //找到第一个不等于arr[mid]的元素
+    if(j-i == maxCount) nums.push_back(arr[mid]); //如果重数是最大重数，则加入数组中
+    else if(j-i > maxCount)  //如果比最大重数要多，更新最大重数，并且之前记录的众数都要删除
     {
-        nums.clear();
+        nums.clear(); //删除之前重数较小的数
         nums.push_back(arr[mid]);
         maxCount = j-i;
     }
-    SearchCountMax(arr,l,i-1);
+    SearchCountMax(arr,l,i-1); //递归
     SearchCountMax(arr,j,r);
 }
+
 
 void SearchCountMax(int* arr,int sz)
 {
